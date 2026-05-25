d79a71399b8e6fd3dab2dbd101923eab3fcdf458
emeraldr8k-wq <emeraldr8k@gmail.com>
2026-05-25 16:02:58 +0300

    чистка

--- app/src/main/java/edu/itschool/abitpro/data/network/AbitApiService.kt
+++ app/src/main/java/edu/itschool/abitpro/data/network/AbitApiService.kt
@@ -1,7 +1,7 @@
 package edu.itschool.abitpro.data.network
 
 import edu.itschool.abitpro.data.dto.HeiDto
-import edu.itschool.abitpro.domain.model.Hei
+
 import retrofit2.http.GET
 
 interface AbitApiService {
